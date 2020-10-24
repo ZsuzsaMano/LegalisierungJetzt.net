73ae9784416baa3f26b9189488822f44cbfad7df
Mano <manoaway@gmail.com>
2020-10-24 23:36:40 +0200

    import all to App.js for test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import './styles/style.min.css';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 import Events from './components/Events';
 import Letter from './components/Letter';
+import Main from './components/Main';
+import Materials from './components/Materials';
 
 function App() {
   return (
     <div className="App">
-<Letter/>
+<Hero/>
     </div>
   );
 }
