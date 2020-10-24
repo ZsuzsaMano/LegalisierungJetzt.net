8bdc94fa7d7746e8032e988546985feae6bd46c6
Mano <manoaway@gmail.com>
2020-10-24 21:09:30 +0200

    add className

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Events = () => {
   return(
-    <main class="event">
+    <main className="event">
 
-        <h2 class="event__title title" id="event__title">Events</h2>
-      <section class="dark-box">
+        <h2 className="event__title title" id="event__title">Events</h2>
+      <section className="dark-box">
         <p>12.10.2020, 11h Aquarium Berlin Presskonferenz</p>
       </section>
 
