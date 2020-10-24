58bd6098e6a63603e221209c59cc47200325058a
Mano <manoaway@gmail.com>
2020-10-24 21:12:52 +0200

    add jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './styles/style.min.css';
 import Events from './components/Events';
+import Letter from './components/Letter';
 
 function App() {
   return (
     <div className="App">
-<Events/>
+<Letter/>
     </div>
   );
 }
---
+++ src/components/Letter.js
@@ -0,0 +1,188 @@
+import React from 'react';
+
+const Letter = () => {
+  return(
+    <main className="letter">
+      <article className="letter__article">
+        <h2 className="letter__title title" id="title">Offener Brief <br/> für die dauerhafte,
+          bedingungslose Legalisierung aller undokumentierten,
+          sich in illegalisierten Bedingungen befindlichen
+          migrantischen Personen
+          </h2>
+        <p className="letter__text dark-box" id='letter__text'>
+          Der aktuelle Kontext drängt uns heute dazu, als Netzwerk von
+          Kollektiven und Organisationen von Migrant*innen und Anti-Rassist*innen
+          die dauerhafte und bedingungslose Legalisierung aller migrantischen
+          Personen ohne Aufenthaltsgenehmigung in diesem Land zu fordern.<br/><br/>
+
+          In Berlin leben schätzungsweise zwischen 60.000 und 100.000 migrantische
+          Personen ohne Dokumente. Unterschiedlichste Gründe führen dazu, dass sie
+          sich in dieser Situation befinden.<br/><br/>
+
+          Ihre Lebensbedingungen sind gezeichnet von Unsichtbarkeit und Prekarität.
+          Es fehlt der Zugang zum Gesundheitswesen, dem Wohnungsmarkt, dem Schulsystem,
+          würdiger Arbeit und einem freien Leben. Offensichtlich werden also
+          grundlegende Menschenrechte nicht für alle hier lebenden Menschen
+          umgesetzt.<br/><br/>
+
+          Ein Großteil der Personen, die unter dieser illegalisierten Situation leiden,
+          kommt aus dem globalen Süden. Vielen wird aus weißer Perspektive die
+          Zugehörigkeit zu anderen 'Rassen' zugeschrieben. Die Lebensrealität
+          von Frauen* und non-binären Personen mit weiblich gelesenen Körpern
+          ist unter den ohnehin schweren Bedingungen besonders problematisch.
+          So verfügen sie beispielsweise über keinerlei Möglichkeiten,
+          Vorfälle sexistischer Gewalt anzuzeigen und Schadensersatz
+          zu erhalten. Indessen sind illegalisierte Männer* etwa bei der
+          Arbeitssuche an öffentlichen Orten vermehrt rassistischen
+          Polizeikontrollen nach den Prinzipien des “Racial Profiling”
+          sowie anderem Missbrauch durch diese Institution ausgesetzt.<br/><br/>
+
+          Wir wollen an dieser Stelle betonen, dass die Ungleichheit zwischen dem
+          Globalen Süden und dem Globalen Norden eine historische Realität ist,
+          welche die Übernahme politischer Verantwortung durch die europäischen
+          Regierungen erfordert. Stattdessen schließen Politiker*innen der EU
+          weiterhin gewinnbringende Verträge über die Ausbeutung natürlicher
+          Ressourcen zum Nachteil der Völker und Gebiete des Globalen Südens ab.
+          Dazu werden rückschrittige Migrationspolitiken ausgebaut, welche das
+          Menschenrecht der Bewegungsfreiheit für die Mehrheit beschneiden.
+          Waren können internationale Grenzen überschreiten, Personen sollen
+          dies jedoch nicht können.<br/><br/>
+
+          So werden Menschen illegalisiert und damit kriminalisiert. Sie werden zum
+          perfekten Ziel sowohl legaler als auch illegaler Ausbeutungsaktivitäten im
+          Interesse großer Unternehmen sowie der deutschen Wirtschaft im Allgemeinen
+          gemacht. Wir sprechen hier von essentiellen Tätigkeiten in der Carearbeit
+          (Pflege, Kinderbetreung, Reinigung etc.) und weiteren Bereichen des
+          tertiären Dienstleistungssektors, in der Landwirtschaft, dem Bauwesen
+          und anderen Branchen.<br/><br/>
+
+          Wir haben es in Bezug auf illegalisierte Personen in Deutschland mit zwei
+          zentralen Problemen zu tun: Zum einen die Kontrolle und Repression durch
+          die Polizei, zum anderen die Tabuisierung des Themas in der deutschen
+          Gesellschaft. Beides macht diesen Bevölkerungsanteil unsichtbar. Ein
+          Resultat dieser Unsichtbarkeit ist das Fehlen quantitativer und qualitativer
+          Daten, welches es uns unmöglich macht, zu wissen, um wie viele Personen es
+          sich handelt, wo sie sich aufhalten und was ihre dringlichsten Bedürfnisse
+          sind.<br/><br/>
+
+          Eine ähnliche Situation besteht in vielen Ländern der EU.
+          Es ist jedoch hervorzuheben, dass andere Länder im Zuge der COVID19-Krise
+          bereits gehandelt haben. So hat z.B. Portugal alle auf dem Staatsgebiet
+          befindlichen Personen legalisiert und Italien hat spezielle
+          Aufenthaltsgenehmigungen ausgestellt - bis heute ohne jegliche negative
+          Konsequenzen für die lokale Gesellschaft. In weiteren Ländern, u.A.
+          Frankreich und Spanien, sind Kampagnen mit entsprechenden Forderungen
+          im Gange.<br/><br/>
+
+          Es ist jetzt der Moment, das Tabu zu brechen und auch hier in Berlin einen
+          Schritt nach vorne zu gehen für eine Legalisierung mit allen dazugehörigen
+          Rechten für alle migrantischen Personen ohne Dokumente. Dafür fordern wir
+          Folgendes von den verantwortlichen Autoritäten:<br/><br/>
+
+          1. Die besondere, weitgehende und dringende dauerhafte Legalisierung aller
+          Personen in illegalisierten Situationen in Berlin.<br/>
+          2. Dass in diesem Verfahren die migrantischen Personen als Subjekte mit Rechten
+          eine zentrale Rolle spielen und der Zugang zu diesen Rechten und ihre
+          Förderung garantiert werden.<br/>
+          3. Die Legalisierung mittles juristischer Instrumente wie z.B. dem Paragraph
+          §23 (1) des Aufenthaltsgesetzes, welcher illegalisierte migrantische Personen
+          aus humanitären Gründen schützen könnte.<br/>
+          4. Die Abschaffung des Paragraphen §87 des Aufenthaltsgesetzes,
+          welcher fordert, dass Angestellte im öffentlichen Dienst (mit
+          Ausnahme der Bildungseinrichtungen) die Information über einen
+          illegalisierten Status weiterleiten.<br/><br/>
+
+          Die COVID19-Krise hat ein Schlaglicht auf die bestehenden sozialen
+          Ungleichheiten geworfen und diese weiter verschärft. Dabei sind
+          illegalisierte Menschen in besonderem Maße betroffen, weil sie keinen
+          Bürger*innenstatus besitzen und nicht als Subjekte mit Rechten behandelt
+          werden. In diesen Zeiten ist Verantwortung und Solidarität gefordert:
+          Undokumentierte Personen dürfen nicht außen vor bleiben! Deshalb appellieren
+          wir an die Zivilgesellschaft und mögliche Alliierte in politischen Sphären,
+          unseren Forderungen nach einem würdigen Leben für alle Menschen Gehör zu
+          schenken. Dazu möchten wir im Besonderen alle politischen und sozialen
+          Organisationen aufrufen, sich den Initiativen anzuschließen, die im
+          Rahmen dieses Kampfes Vorarbeit geleistet haben!<br/><br/>
+
+          Wir erinnern an dieser Stelle an alle migrantischen und anti-rassistischen
+          Kämpfe, die seit Jahrzehnten die Legalisierung und den Zugang zu Grundrechten
+          in Deutschland fordern. Die Karawane für die Rechte Geflüchteter und anderer
+          Migrant*innen, die Gesellschaft für Legalisierung, zahlreiche Protestmärsche
+          und die Besetzung des Oranienplatzes sind nur einige der Aktivitäten von
+          Gruppen und Netzwerken undokumentierter Menschen. Wir grüßen die Kämpfe für
+          Legalisierung in allen Teilen der Erde und solidarisieren uns mit
+          ihnen aus unserer eigenen Kampagne in Berlin!<br/><br/>
+
+          LEGALISIERUNG JETZT!<br/><br/>
+
+          Die aktuelle Krise zeigt mit größter Klarheit, dass niemand von den
+          fundamentalen sozialen Rechten ausgeschlossen werden darf! Fordert
+          deshalb mit uns die Legalisierung aller Menschen ohne Aufenthaltsdokumente!
+
+        </p>
+      </article>
+      <div className="signatories">
+        <h3 id="signatories">Unterschriften</h3>
+        <p>Respect Berlin,
+          Solidarity City Berlin,
+          La Casita,
+          La Pachangona,
+          Asamblea Mujeres Disidencias Sexuales/Bloque Latinoamericano,
+          Justizwatch,
+          KuB – Kontakt- und Beratungsstelle für Flüchtlinge und Migrant_innen e.V.,
+          Oficina Precaria,
+          LAFI – Lateinamerikanische FrauenInitiative in Neukölln e.V.,
+          No Border Assembly Berlin,
+          Migrationsrat Berlin e.V.,
+          Voces de Guatemala,
+          Corasol,
+          Women in Exile,
+          ABA - Aktionsbündnis Antira,
+          korientation. Netzwerk für Asiatisch-Deutsche Perspektiven e.V.,
+          NaturFreunde Berlin,
+          Die Meute,
+          Sprungbrett Zukunft Berlin e.V.,
+          xart splitta e.V.,
+          Seebrücke Berlin,
+          Flüchtlingsrat Berlin,
+          Medibüro Berlin,
+          Each One Teach One (EOTO) e.V.,
+          TransInterQueer e.V.,
+          Verein Migration, Entwicklung und Partizipation MEPa e.V.,
+          CILIP – Bürgerrechte und Polizei,
+          Berliner Bündnis gegen Rechts,
+          glokal e.V. Berlin,
+          Gabriela Germany,
+          Netzwerk Reproduktive Gerechtigkeit Berlin,
+          kitchen politics,
+          Theater X,
+          Migrantifa Berlin,
+          Anna-Maria Wünst,
+          Unidos por la paz,
+          Radio Matraca,
+          Nachrichten Pool Lateinamerika e.V.,
+          Frauenkreise Berlin,
+          Space2groW,
+          Kolumbienkampagne Berlin,
+          borderline-europe Menschenrechte ohne Grenzen e.V.,
+          Initiative Schwarze Menschen in Deutschland ISD,
+          Death in Custody - Bündnis,
+          International Women Space,
+          Culture of Deportation,
+          Critical Workers Berlin,
+          SolA,
+          Colectivo MAWVN, Berlin,
+          Andrea Molina,
+          LINKS*KANAX Berlin,
+          ReachOut – Opferberatung und Bildung gegen Rechtsextremismus, Rassismus und Antisemitismus
+          Allmende e.V.,
+          RaumErweiterungsHalle,
+          We'll come united Berlin / Brandenburg,
+          association14a,
+          FAU Berlin </p>
+        </div>
+    </main>
+  );
+};
+
+export default Letter;
