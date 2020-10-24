84b708e445aaf29701370ba98beae8b64fc1b4fd
Mano <manoaway@gmail.com>
2020-10-24 23:36:23 +0200

    add hero

---
+++ src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <a href="./index.html">
+    <header>
+      <span>Close the gap</span>
+      <img className="header__logo" src="../img/logo.png" alt="logo"/>
+      <span>Open the borders</span>
+    </header>
+    </a>
+  );
+};
+
+export default Hero;
