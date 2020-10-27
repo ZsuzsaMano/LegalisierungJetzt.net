e92c59c07f7de33e7c84a7fee6028254728be899
Mano <manoaway@gmail.com>
2020-10-27 19:42:22 +0100

    img pass correct to jsx

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Logo from  '../img/logo.png';
 
 const Hero = () => {
   return (
     <a href="./index.html">
     <header>
       <span>Close the gap</span>
-      <img className="header__logo" src="../img/logo.png" alt="logo"/>
+      <img className="header__logo" src={Logo} alt="logo"/>
       <span>Open the borders</span>
     </header>
     </a>
