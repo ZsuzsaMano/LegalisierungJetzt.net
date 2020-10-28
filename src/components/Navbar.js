258ad6193e82bbd3a04a88022fa1d5ecb517b6de
Mano <manoaway@gmail.com>
2020-10-28 09:05:33 +0100

    hover style fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,12 +9,12 @@ const Navbar = ({ updatePage, updateLanguage, language }) =>
             <li id="nav__letter" onClick = {updatePage}>{languagesObject.[language].nav[0]}</li>
             <li id="nav__events" onClick = {updatePage}> {languagesObject.[language].nav[1]}</li>
             <li id="nav__materials" onClick = {updatePage}>{languagesObject.[language].nav[2]}</li>
-                    <li>
+
             <ul className="languages">
                <li id="DE" className="languages__tab" onClick = {updateLanguage}>DE</li>
                <li id="EN" className="languages__tab" onClick = {updateLanguage}>EN</li>
                <li id="ES" className="languages__tab" onClick = {updateLanguage}>ES</li>
-             </ul> </li>
+             </ul> 
         </ul>
       </nav>
   );
