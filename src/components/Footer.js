666ae7eba9913b51025681997adba5297a027e03
Mano <manoaway@gmail.com>
2020-11-18 22:07:33 +0100

    add svg and link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import Twitter from '../img/twitter.svg';
+import Facebook from '../img/facebook.svg';
 
 const Footer = () => {
-  
+  return(
+    <footer>
+    <a href=""><img src={Twitter} alt="Email"/></a>
+    <a href=""><img src={Twitter} alt="Twitter"/></a>
+    <a href=""><img src={Facebook} alt="Facebook"/></a>
+
+    </footer>
+  )
 }
+
+export default Footer;
