2e461a9af181d897f5f4d069335e6d9511ced714
Mano <manoaway@gmail.com>
2020-11-22 21:30:38 +0100

    add youtube embed

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,13 +6,7 @@ const Main = () => {
 
         <h2 className="video__title title">Presskonferenz 12.10.2020, 11h Aquarium Berlin</h2>
 
-        <iframe
-            src="https://www.youtube.com/watch?v=q4HM1sDtz6I&feature=youtu.be"
-            title="press conference"
-            frameBorder="2"
-            scrolling="no"
-            allowFullScreen={true}>
-        </iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/q4HM1sDtz6I" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 
     </main>
