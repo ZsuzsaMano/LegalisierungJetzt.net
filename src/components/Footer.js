2cc36c9575e003c7ea277ddcc4e5cc17a1020d6e
Mano <manoaway@gmail.com>
2020-11-22 21:17:39 +0100

    add target _blank

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ import Facebook from '../img/facebook.svg';
 const Footer = () => {
   return(
     <footer>
-    <a href="legalisierung_jetzt@riseup.net">Email</a>
-    <a href="https://www.facebook.com/legalisierungjetzt/">Facebook</a>
+    <a href="legalisierung_jetzt@riseup.net" target="_blank">Email</a>
+    <a href="https://www.facebook.com/legalisierungjetzt/" target="_blank">Facebook</a>
 
-    <a href="https://twitter.com/legaljetzt">Twitter</a>
-    <a href="https://www.instagram.com/legalisierung_jetzt/">Instagram</a>
+    <a href="https://twitter.com/legaljetzt" target="_blank">Twitter</a>
+    <a href="https://www.instagram.com/legalisierung_jetzt/" target="_blank">Instagram</a>
 
     </footer>
   )
