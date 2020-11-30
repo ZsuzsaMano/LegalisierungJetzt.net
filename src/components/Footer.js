aa67d1c79d20d5e701ae3f5b25989b822e423e1a
Mano <manoaway@gmail.com>
2020-11-30 19:14:28 +0100

    add spendekonto

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,26 @@ import Facebook from '../img/facebook.svg';
 const Footer = () => {
   return(
     <footer>
+    <div className="footer__social">
     <a href="legalisierung_jetzt@riseup.net" target="_blank">Email</a>
     <a href="https://www.facebook.com/legalisierungjetzt/" target="_blank">Facebook</a>
 
     <a href="https://twitter.com/legaljetzt" target="_blank">Twitter</a>
     <a href="https://www.instagram.com/legalisierung_jetzt/" target="_blank">Instagram</a>
+</div>
+<div className="footer__spende">
+<p>
+Spendenkonto <br/>
+Respect Berlin <br/>
+DE34 8306 5408 0004 9035 60 <br/>
+BIC GENO DEF1 SLR <br/>
+Deutsche Skatbank <br/>
+Verwendungszweck: Legalisierungjetzt <br/><br/>
 
+Spenden sind leider nicht steuerlich absetzbar. Sie fließen in die Umsetzung der Kampagne für Legalisierung 2020/21.</p>
+</div>
     </footer>
+
   )
 }
 
