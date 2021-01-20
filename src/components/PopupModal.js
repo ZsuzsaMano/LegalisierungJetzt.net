4d2af9a945f7be5e683a8152b18a8693e736923f
Johanna Galarza <johanna.galarza@relayr.io>
2021-01-20 18:35:43 +0100

    commenting out i18n more

--- src/components/PopupModal.js
+++ src/components/PopupModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { useTranslation } from 'react-i18next';
+//import { useTranslation } from 'react-i18next';
 import { LegalisierungContext } from '../context/LegalisierungContext';
 
 const PopupModal = (props) => {
