023377c16f9f908675da762ec5ae7580a35bcb20
felipeloha <felipe@pulpowms.com>
2021-01-21 21:46:22 +0100

    signatures in columns

--- src/components/Letter.js
+++ src/components/Letter.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-array-index-key */
 import React, { useContext } from 'react';
 import { LegalisierungContext } from '../context/LegalisierungContext';
 import languagesObject from '../languagesObject';
@@ -5,6 +6,69 @@ import languagesObject from '../languagesObject';
 const Letter = () => {
   const { language } = useContext(LegalisierungContext);
 
+  const signatures = [
+    'Respect Berlin',
+    'Solidarity City Berlin',
+    'La Casita',
+    'La Pachangona',
+    'Asamblea Mujeres Disidencias Sexuales/Bloque Latinoamericano',
+    'Justizwatch',
+    'KuB – Kontakt- und Beratungsstelle für Flüchtlinge und Migrant_innen e.V.',
+    'Oficina Precaria',
+    'LAFI – Lateinamerikanische FrauenInitiative in Neukölln e.V.',
+    'No Border Assembly Berlin',
+    'Migrationsrat Berlin e.V.',
+    'Voces de Guatemala',
+    'Corasol',
+    'Women in Exile',
+    'ABA - Aktionsbündnis Antira',
+    'korientation. Netzwerk für Asiatisch-Deutsche Perspektiven e.V.',
+    'NaturFreunde Berlin',
+    'Die Meute',
+    'Sprungbrett Zukunft Berlin e.V.',
+    'xart splitta e.V.',
+    'Seebrücke Berlin',
+    'Flüchtlingsrat Berlin',
+    'Medibüro Berlin',
+    'Each One Teach One (EOTO) e.V.',
+    'TransInterQueer e.V.',
+    'Verein Migration, Entwicklung und Partizipation MEPa e.V.',
+    'CILIP – Bürgerrechte und Polizei',
+    'Berliner Bündnis gegen Rechts',
+    'glokal e.V. Berlin',
+    'Gabriela Germany',
+    'Netzwerk Reproduktive Gerechtigkeit Berlin',
+    'kitchen politics',
+    'Theater X',
+    'Migrantifa Berlin',
+    'Anna-Maria Wünst',
+    'Unidos por la paz',
+    'Radio Matraca',
+    'Nachrichten Pool Lateinamerika e.V.',
+    'Frauenkreise Berlin',
+    'Space2groW',
+    'Kolumbienkampagne Berlin',
+    'borderline-europe Menschenrechte ohne Grenzen e.V.',
+    'Initiative Schwarze Menschen in Deutschland ISD',
+    'Death in Custody - Bündnis',
+    'International Women Space',
+    'Culture of Deportation',
+    'Critical Workers Berlin',
+    'SolA',
+    'Colectivo MAWVN, Berlin',
+    'Andrea Molina',
+    'LINKS*KANAX Berlin',
+    'ReachOut – Opferberatung und Bildung gegen Rechtsextremismus, Rassismus und Antisemitismus',
+    'Allmende e.V.',
+    'RaumErweiterungsHalle',
+    "We'll come united Berlin / Brandenburg",
+    'association14a',
+    'FAU Berlin',
+    'Justizwatch, Lampedusa Berlin',
+    'unter_bau',
+    'trixiewiz e.V.',
+    'Sudan Uprising Germany Team',
+  ];
   return (
     <main className="letter">
       <article className="letter__article">
@@ -17,69 +81,11 @@ const Letter = () => {
       </article>
       <div className="signatories">
         <h3 id="signatories">{languagesObject.[language].signatories}</h3>
-        <p>
-          {`Respect Berlin,
-          Solidarity City Berlin,
-          La Casita,
-          La Pachangona,
-          Asamblea Mujeres Disidencias Sexuales/Bloque Latinoamericano,
-          Justizwatch,
-          KuB – Kontakt- und Beratungsstelle für Flüchtlinge und Migrant_innen e.V.,
-          Oficina Precaria,
-          LAFI – Lateinamerikanische FrauenInitiative in Neukölln e.V.,
-          No Border Assembly Berlin,
-          Migrationsrat Berlin e.V.,
-          Voces de Guatemala,
-          Corasol,
-          Women in Exile,
-          ABA - Aktionsbündnis Antira,
-          korientation. Netzwerk für Asiatisch-Deutsche Perspektiven e.V.,
-          NaturFreunde Berlin,
-          Die Meute,
-          Sprungbrett Zukunft Berlin e.V.,
-          xart splitta e.V.,
-          Seebrücke Berlin,
-          Flüchtlingsrat Berlin,
-          Medibüro Berlin,
-          Each One Teach One (EOTO) e.V.,
-          TransInterQueer e.V.,
-          Verein Migration, Entwicklung und Partizipation MEPa e.V.,
-          CILIP – Bürgerrechte und Polizei,
-          Berliner Bündnis gegen Rechts,
-          glokal e.V. Berlin,
-          Gabriela Germany,
-          Netzwerk Reproduktive Gerechtigkeit Berlin,
-          kitchen politics,
-          Theater X,
-          Migrantifa Berlin,
-          Anna-Maria Wünst,
-          Unidos por la paz,
-          Radio Matraca,
-          Nachrichten Pool Lateinamerika e.V.,
-          Frauenkreise Berlin,
-          Space2groW,
-          Kolumbienkampagne Berlin,
-          borderline-europe Menschenrechte ohne Grenzen e.V.,
-          Initiative Schwarze Menschen in Deutschland ISD,
-          Death in Custody - Bündnis,
-          International Women Space,
-          Culture of Deportation,
-          Critical Workers Berlin,
-          SolA,
-          Colectivo MAWVN, Berlin,
-          Andrea Molina,
-          LINKS*KANAX Berlin,
-          ReachOut – Opferberatung und Bildung gegen Rechtsextremismus, Rassismus und Antisemitismus
-          Allmende e.V.,
-          RaumErweiterungsHalle,
-          We'll come united Berlin / Brandenburg,
-          association14a,
-          FAU Berlin,
-          Justizwatch, Lampedusa Berlin,
-          unter_bau,
-          trixiewiz e.V.,
-          Sudan Uprising Germany Team`}
-        </p>
+        <ul className="signatureList">
+          {signatures.map((signature, i) => (
+            <li className="signatureList" key={`key-${i}`}>{signature}</li>
+          ))}
+        </ul>
       </div>
     </main>
   );
