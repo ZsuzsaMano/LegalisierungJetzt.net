edf81e5ff8a67754791ce87aec1920314b431810
felipeloha <felipe@pulpowms.com>
2021-03-14 16:04:09 +0100

    change link color

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -2,11 +2,23 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useContext, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Button, Popover } from '@material-ui/core';
+import { makeStyles, Button, Popover } from '@material-ui/core';
 import LanguageIcon from '@material-ui/icons/Language';
 import { LegalisierungContext } from '../context/LegalisierungContext';
 
+const useStyles = makeStyles(() => ({
+  link: {
+    fill: 'white',
+    zIndex: '99999',
+    '&:hover': {
+      transition: 'all 200ms ease-in',
+      fill: '#D32AA2',
+    },
+  },
+}));
+
 const LanguageSelector = () => {
+  const { link } = useStyles();
   const { i18n } = useTranslation();
 
   const { setLanguage } = useContext(LegalisierungContext);
@@ -31,7 +43,7 @@ const LanguageSelector = () => {
   return (
     <div className="popup">
       <Button aria-describedby={id} onClick={toggleLanguage}>
-        <LanguageIcon style={{ fill: 'white', zIndex: '99999' }} />
+        <LanguageIcon className={link} />
       </Button>
       <Popover
         id={id}
